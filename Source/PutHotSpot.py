04bec69844a5839e22cd9c6cb9aa11b7816d46a9
John Konderla <konderla.john@gmail.com>
2018-11-11 20:38:49 -0500

    Added ability to update dots on PUT command

--- Source/PutHotSpot.py
+++ Source/PutHotSpot.py
@@ -1,5 +1,5 @@
+import datetime
 import os
-from datetime import datetime
 from decimal import Decimal
 
 import boto3
@@ -13,15 +13,33 @@ def put_hot_spot_handler(event, context):
     table = dynamodb.Table(os.getenv('TABLE_NAME'))
     lat = Decimal(str(event.get('lat', 35.7721)))
     lng = Decimal(str(event.get('lng', -78.6441)))
-    id = context.aws_request_id
-    hash = str(event.get('hash', 'no hash'))
+    location_id = event.get('locationID', '')
+    input_hash = str(event.get('hash', 'no hash'))
     print("lat, lng: {} {}".format(lat, lng))
-    date_time = datetime.now()
+    date_time = datetime.datetime.now()
     color_code = event.get('colorCode', '1')
 
     key = '{:.1f}_{:.1f}'.format(lat, lng)
     print('key: {}'.format(key))
     response = {}
+
+    if location_id:
+        print('looking for a location: {}'.format(location_id))
+        two_hours_less = datetime.timedelta(hours=2)
+        minus_two = date_time - two_hours_less
+        query_response = table.query(IndexName='locationID-date_time-index',
+                                     KeyConditionExpression='locationID = :location_id and date_time > :date_time_value',
+                                     FilterExpression='lat_lng = :lat_lng_value',
+                                     ExpressionAttributeValues={
+                                         ":location_id": location_id,
+                                         ":date_time_value": '{}'.format(minus_two),
+                                         ':lat_lng_value': key
+                                     })
+        print('response.... {}'.format(query_response))
+        if not query_response['Items']:
+            location_id = context.aws_request_id
+    else:
+        location_id = context.aws_request_id
 
     try:
         response = table.put_item(
@@ -30,8 +48,8 @@ def put_hot_spot_handler(event, context):
                 'lat': lat,
                 'lng': lng,
                 'date_time': "{}".format(date_time),
-                'locationID': id,
-                'hash': hash,
+                'locationID': location_id,
+                'hash': input_hash,
                 'colorCode': color_code
             }
         )
@@ -42,7 +60,7 @@ def put_hot_spot_handler(event, context):
     if response:
         return {
             "response": "success",
-            "locationID": id
+            "locationID": location_id
         }
     else:
         return {"message": "error"}
@@ -51,15 +69,16 @@ def put_hot_spot_handler(event, context):
 if __name__ == '__main__':
     class Context:
         def __init__(self):
-            self.aws_request_id = '123'
+            self.aws_request_id = '123123123123'
 
 
-    os.environ['TABLE_NAME'] = "HotSpotAlpha"
+    os.environ['TABLE_NAME'] = "HotSpotDynamoDbTable-HotSpotTable-F6VB59AJ2YDK"
 
     event_dict = {
         'lat': 123.2,
         'lng': 321.1,
-        'colorCode': 1
+        'colorCode': 1,
+        "locationID": '123123123123'
 
     }
     context = Context()
