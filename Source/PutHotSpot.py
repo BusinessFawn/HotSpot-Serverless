2b2c98032d772324c4fe74d01de011f320a0346d
John Konderla <konderla.john@gmail.com>
2018-11-25 13:15:56 -0500

    Added some additional functionality for dot replacement

--- Source/PutHotSpot.py
+++ Source/PutHotSpot.py
@@ -5,42 +5,50 @@ from decimal import Decimal
 import boto3
 from botocore.exceptions import ClientError
 
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.getenv('TABLE_NAME'))
+
 
 def put_hot_spot_handler(event, context):
-    print('event: {}'.format(event))
-    dynamodb = boto3.resource("dynamodb")
+    print('starting to read.. event: {}'.format(event))
+    updated_existing_item = False
+    if event.get('locationID', ''):
+        print('found locationID: {}'.format(event.get('locationID')))
+        updated_existing_item = update_existing_item(event)
 
-    table = dynamodb.Table(os.getenv('TABLE_NAME'))
-    lat = Decimal(str(event.get('lat', 35.7721)))
-    lng = Decimal(str(event.get('lng', -78.6441)))
-    location_id = str(event.get('locationID', ''))
-    input_hash = str(event.get('hash', ''))
-    print("lat, lng: {} {}".format(lat, lng))
-    date_time = datetime.datetime.now()
-    color_code = event.get('colorCode', 1)
+    if not updated_existing_item:
+        print('item not updatedable, moving on with new locationID')
+        event['locationID'] = context.aws_request_id
+    return put_new_location(event)
 
-    key = '{:.1f}_{:.1f}'.format(lat, lng)
-    print('key: {}'.format(key))
-    response = {}
 
-    if location_id:
-        print('looking for a location: {}'.format(location_id))
-        two_hours_less = datetime.timedelta(hours=2)
-        minus_two = date_time - two_hours_less
-        query_response = table.query(IndexName='locationID-date_time-index',
-                                     KeyConditionExpression='locationID = :location_id and date_time > :date_time_value',
-                                     FilterExpression='lat_lng = :lat_lng_value',
-                                     ExpressionAttributeValues={
-                                         ":location_id": location_id,
-                                         ":date_time_value": '{}'.format(minus_two),
-                                         ':lat_lng_value': key
-                                     })
-        print('response.... {}'.format(query_response))
-        if not query_response['Items']:
-            location_id = context.aws_request_id
-    else:
-        location_id = context.aws_request_id
+def update_existing_item(input_dict: dict):
+    lat, lng, location_id, input_hash, date_time, color_code, key = unpack_values(input_dict)
 
+    print('looking for a location: {}'.format(location_id))
+    two_hours_less = datetime.timedelta(hours=2)
+    minus_two = date_time - two_hours_less
+    query_response = table.query(IndexName='locationID-date_time-index',
+                                 KeyConditionExpression='locationID = :location_id and date_time > :date_time_value',
+                                 FilterExpression='lat_lng = :lat_lng_value',
+                                 ExpressionAttributeValues={
+                                     ":location_id": location_id,
+                                     ":date_time_value": '{}'.format(minus_two),
+                                     ':lat_lng_value': key
+                                 })
+    print('response.... {}'.format(query_response))
+    if not query_response['Items']:
+        return False
+    print('found Items...')
+    old_date_time = query_response['Items'][0]['date_time']
+    del_response = table.delete_item(Key={'lat_lng': key, 'date_time': old_date_time})
+    print('del response: {}'.format(del_response))
+    return True
+
+
+def put_new_location(input_dict: dict):
+    lat, lng, location_id, input_hash, date_time, color_code, key = unpack_values(input_dict)
+    response = ''
     try:
         new_put_item = {
             'lat_lng': key,
@@ -71,6 +79,22 @@ def put_hot_spot_handler(event, context):
     return return_dict
 
 
+def unpack_values(input_dict: dict) -> tuple:
+    print('event: {}'.format(input_dict))
+    lat = Decimal(str(input_dict.get('lat', 35.7721)))
+    lng = Decimal(str(input_dict.get('lng', -78.6441)))
+    location_id = str(input_dict.get('locationID', ''))
+    input_hash = str(input_dict.get('hash', ''))
+    print("lat, lng: {} {}".format(lat, lng))
+    date_time = datetime.datetime.now()
+    color_code = input_dict.get('colorCode', 1)
+
+    key = '{:.1f}_{:.1f}'.format(lat, lng)
+    print('key: {}'.format(key))
+
+    return lat, lng, location_id, input_hash, date_time, color_code, key
+
+
 if __name__ == '__main__':
     class Context:
         def __init__(self):
@@ -86,5 +110,5 @@ if __name__ == '__main__':
         "locationID": '123123123123'
 
     }
-    context = Context()
-    print(put_hot_spot_handler(event_dict, context))
+    test_context = Context()
+    print(put_hot_spot_handler(event_dict, test_context))
